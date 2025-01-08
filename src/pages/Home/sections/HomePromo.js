321157656b649863eb1c39ef4f4e6063806a1f3c
jarvis <maxabdurasulov@gmail.com>
2025-01-08 18:24:49 +0600

    lazy HomePromoSwiper removed

--- src/pages/Home/sections/HomePromo.js
+++ src/pages/Home/sections/HomePromo.js
@@ -1,10 +1,10 @@
 import { styled } from '@mui/material';
-import Loadable from 'components/Loadable';
-import React, { lazy } from 'react';
+import React from 'react';
+import HomePromoSwiper from '../components/HomePromoSwiper';
 
-const HomePromoSwiper = Loadable(
-  lazy(() => import('../components/HomePromoSwiper')),
-);
+// const HomePromoSwiper = Loadable(
+//   lazy(() => import('../components/HomePromoSwiper')),
+// );
 
 const StyledSection = styled('section')(({ theme }) => ({
   position: 'relative',
