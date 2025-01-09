b710e5c3954d29b05efc9cd11caea5fb193c637f
jarvis <maxabdurasulov@gmail.com>
2025-01-09 14:10:31 +0600

    changed about bg and mission image

--- src/pages/About/sections/AboutSection4.js
+++ src/pages/About/sections/AboutSection4.js
@@ -1,5 +1,5 @@
 import { Grid, styled, Typography, useTheme } from '@mui/material';
-import OurTeamImg from 'assets/images/mission.webp';
+import OurTeamImg from 'assets/images/mission2.webp';
 import LazyImage from 'components/LazyImage';
 import OrionContainer from 'components/OrionContainer';
 import React from 'react';
