4f76f87b6ec0aac4f5c6473b00dd59cd7a0ee05a
jarvis <maxabdurasulov@gmail.com>
2025-01-14 15:33:49 +0600

    react-image removed

--- src/components/LazyImage.js
+++ src/components/LazyImage.js
@@ -1,17 +1,33 @@
+import React, { useState } from 'react';
 import { CircularProgress } from '@mui/material';
-import React from 'react';
-import { Img } from 'react-image';
 
 const LazyImage = ({ src, alt = 'LazyImage', ...props }) => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
   return (
-    <Img
-      src={src}
-      alt={alt}
-      {...props}
-      loading="lazy"
-      unloader={<div>Error</div>}
-      loader={<CircularProgress />}
-    />
+    <div style={{ position: 'relative', display: 'inline-block' }}>
+      {loading && !error && (
+        <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
+          <CircularProgress />
+        </div>
+      )}
+      {!error && (
+        <img
+          src={src}
+          alt={alt}
+          {...props}
+          loading="lazy"
+          onLoad={() => setLoading(false)}
+          onError={() => {
+            setLoading(false);
+            setError(true);
+          }}
+          style={loading ? { visibility: 'hidden' } : {}}
+        />
+      )}
+      {error && <div style={{ color: 'red' }}>Error loading image</div>}
+    </div>
   );
 };
 
