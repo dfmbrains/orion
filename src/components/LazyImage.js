4487cced9a5613f9f6c04e8fad990337298f6493
jarvis <maxabdurasulov@gmail.com>
2025-01-14 18:06:21 +0600

    lazy image => html image

--- src/components/LazyImage.js
+++ src/components/LazyImage.js
@@ -1,34 +1,7 @@
-import React, { useState } from 'react';
-import { CircularProgress } from '@mui/material';
+import React from 'react';
 
 const LazyImage = ({ src, alt = 'LazyImage', ...props }) => {
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
-
-  return (
-    <div style={{ position: 'relative', display: 'inline-block' }}>
-      {loading && !error && (
-        <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
-          <CircularProgress />
-        </div>
-      )}
-      {!error && (
-        <img
-          src={src}
-          alt={alt}
-          {...props}
-          loading="lazy"
-          onLoad={() => setLoading(false)}
-          onError={() => {
-            setLoading(false);
-            setError(true);
-          }}
-          style={loading ? { visibility: 'hidden' } : {}}
-        />
-      )}
-      {error && <div style={{ color: 'red' }}>Error loading image</div>}
-    </div>
-  );
+  return <img src={src} alt={alt} {...props} loading="lazy" />;
 };
 
 export default LazyImage;
